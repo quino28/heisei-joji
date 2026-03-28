3dc3f4bfead28bb93ca5f1f8e591161aaff8ee6b
RinM <quinolactacin@gmail.com>
2026-03-28 19:37:08 +0200

    first commit

---
+++ app/components/Footer.tsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+    <footer
+      className="w-full h-16 flex items-center justify-center"
+      style={{
+        backgroundColor: '#998DBD',
+        height: '50px',
+      }}
+    >
+      <p>Developed by quino.</p>
+    </footer>
+  );
+}
---
+++ app/components/Header.tsx
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <header
+      className="fixed top-0 left-0 right-0 z-50 h-16 flex items-center px-6"
+      style={{
+        backgroundColor: '#998DBD',
+        height: '50px',
+      }}
+    >
+      <p>header</p>
+    </header>
+  );
+}
